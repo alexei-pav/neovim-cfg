0b7cd0f2a5dbc9f251b93ff3a06a4413c179e14a
alexei-pav <loshapavlov@gmail.com>
2024-10-03 19:01:57 +0200

    nothing

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -12,8 +12,8 @@ vim.fn.sign_define("DiagnosticSignHint",
 require('neo-tree').setup {
   filesystem = {
     filtered_items = {
-      visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
-      hide_dotfiles = false,
+      visible = true,
+      hide_dotfiles = false, -- Show hidden files
       hide_gitignored = false,
     }
   }
