b61f384d3dd6ffe49ce1655cbf2fe094c5b18551
alexei-pav <loshapavlov@gmail.com>
2024-10-05 12:35:22 +0200

    install colorscheme

--- init.lua
+++ init.lua
@@ -3,11 +3,11 @@
 require("core.lazy")          -- Packet manager
 require("core.mappings")      -- Shortcuts
 require("core.configs")
---require("core.colors")        -- Color
+require("core.colors")        -- Color
 
 -- Plugins
 
-require('plugins.neotree')	    -- View files
+--require('plugins.neotree')	    -- View files
 --require('plugins.treesitter')	-- Highlights
 --require('plugins.lsp')
 --require("plugins.cmp")
@@ -15,5 +15,5 @@ require('plugins.neotree')	    -- View files
 --require("plugins.telescope")	-- Look for files and text
 --require("plugins.none-ls")
 --require("plugins.bufferline")	-- Tabs
-require("plugins.dashboard")	  -- Dashboard
+--require("plugins.dashboard")	  -- Dashboard
 --require("plugins.toggleterm")	-- Terminal emulator
